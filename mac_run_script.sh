3d937fd7f98ec237c5f57598e1c2e9bd031f37d4
Molly Machin <Open>
2025-02-19 11:43:15 +0000

    Made a script to run the app on macOS

---
+++ mac_run_script.sh
@@ -0,0 +1,16 @@
+(cd ../stars_backend; uvicorn database_service:app --host 127.0.0.1 --port 5000 --reload 1> "backend_log.txt" 2> "backend_error_log.txt") &
+(cd ../StarsApplication; uvicorn main:app --host 127.0.0.1 --port 8000 --reload 1> "app_log.txt" 2> "app_error_log.txt") &
+(cd ../StarsFrontend/static; python3 -m http.server 3000 1> "frontend_log.txt" 2> "frontend_error_log.txt") &
+
+echo "http://localhost:3000"
+
+read -p "(Don't Ctrl + C!) Press any other key to quit ..." -n1 -s
+
+sudo lsof -i :8000 -i :5000 -i :3000 | 
+    awk 'NR > 1 {print $2}' |
+    sort -u |
+    while read line ; do
+      kill -9 "$line"
+    done
+
+echo  "Processes terminated succesfully!"
