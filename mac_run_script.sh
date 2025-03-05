711210f29e16e87d7735044641d5f25aa6c9bcc0
Molly Machin <Open>
2025-03-05 15:45:29 +0000

    Updated port number on runner script

--- mac_run_script.sh
+++ mac_run_script.sh
@@ -1,6 +1,6 @@
 (cd ../stars_backend_new; uvicorn database_service:app --host 127.0.0.1 --port 5000 --reload 1> "backend_log.txt" 2> "backend_error_log.txt") &
 (cd ../Authentication; uvicorn auth_service:app --host 127.0.0.1 --port 5001 --reload 1> "auth_log.txt" 2> "auth_error_log.txt") &
-(cd ../StarsApplication; uvicorn main:app --host 127.0.0.1 --port 8000 --reload 1> "app_log.txt" 2> "app_error_log.txt") &
+(cd ../StarsApplication; uvicorn main:app --host 127.0.0.1 --port 7999 --reload 1> "app_log.txt" 2> "app_error_log.txt") &
 (cd ../StarsFrontend/static; python3 -m http.server 3000 1> "frontend_log.txt" 2> "frontend_error_log.txt") &
 
 echo "http://localhost:3000"
